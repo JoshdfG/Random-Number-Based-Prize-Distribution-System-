880e10103128e11f514d0fde8b2855f0f94ecb1e
JoshdfG <agboolajoshua161@gmail.com>
2024-02-29 21:28:03 +0100

    first commit

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,5 +1,6 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
+require("hardhat-import-from-npm");
 
 const config: HardhatUserConfig = {
   solidity: "0.8.24",
